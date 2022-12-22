deff82aaab023143195a15e85de3d340c7353c44
Vorobeva Irina <isvorobyeva@mail.ru>
2022-12-22 15:22:26 +0300

    start

---
+++ src/main/java/tv/lid/cinema/api/App.java
@@ -0,0 +1,13 @@
+package tv.lid.cinema.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
